23ad187f64e2d73555c37527db2ba68fdb86f6fa
ltakavar <ltakavar@ford.com>
2024-11-22 15:53:30 +0000

    basic additon of summart endpoint

--- app.py
+++ app.py
@@ -41,6 +41,19 @@ def birds():
 
     else:
         return jsonify({'error': 'Method not allowed'}), 405
+    
+@app.route('/summary', methods=['POST'])
+def summary():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if not data:
+            return jsonify('erorr: No JSON data provided')
+        
+        # This IP can be user with libraries like "Flask-Simple-GeoIP" to map the IP to a region
+        ip = request.remote_addr
+
+        return jsonify("request accepted"), 202
 
 if __name__ == '__main__':
     app.run(port=5000)
