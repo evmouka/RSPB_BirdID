903195c2c88184807bab157fa4066286bc38d6bf
emile <guaitanverish2@outlook.com>
2024-11-23 01:38:49 +0000

    updated frontend

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import Chat from "./sections/ChatLogic"
+import Header from "./sections/header"
+
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Chat />
+    </>
+  )
+}
+
+export default App;
