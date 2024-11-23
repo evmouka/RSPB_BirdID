92335172b36028e53df85bdf9787e035e00e5f4d
ltakavar <ltakavar@ford.com>
2024-11-23 11:35:43 +0000

    added result logic for bird found

---
+++ src/components/birdIdentity.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+interface BirdIdentityProps {
+  imageSrc: string; 
+  birdName: string;
+}
+
+const BirdIdentity = ({imageSrc, birdName}: BirdIdentityProps) => {
+    
+    function onButtonClick(){
+        return "clicked"
+    };
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <img
+        src={imageSrc}
+        alt="Descriptive Alt Text"
+        style={{ maxWidth: "300px", height: "300px", marginBottom: "20px", borderRadius: "50%" }}
+      />
+      <h1 style={{ margin: "10px 0", fontSize: "xx-large" }}>New Species Identified!</h1> {/* Static header */}
+      <h2 style={{ margin: "10px 0", fontSize: "xx-large", fontWeight: "bold" }}>{birdName}</h2>
+      <button
+        onClick={onButtonClick}
+        style={{
+          padding: "10px 20px",
+          backgroundColor: "#57A627",
+          color: "#fff",
+          border: "none",
+          borderRadius: "9999px",
+          cursor: "pointer",
+          display: "inline-flex",
+          alignItems: "center",
+          height: "50px",
+          gap: "8px",
+        }}
+      >
+        <img 
+          src="../../public/Plus.svg" 
+          alt="Add" 
+          width="15" 
+          height="15" />
+        Add to my list
+      </button>
+    </div>
+  );
+};
+
+export default BirdIdentity;
